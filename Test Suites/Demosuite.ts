c4d89bb513c410d09655af69bbe59c75d0d43d99
KSS <KSS@192.168.27.106>
2018-12-12 22:53:47 +0530

    Change-Id: Ic6fee511ded73ad58a81b4d7566895ef38d44b5f Signed-off-by: KSS <KSS@192.168.27.106>

--- Test Suites/Demosuite.ts
+++ Test Suites/Demosuite.ts
@@ -4,7 +4,7 @@
    <name>Demosuite</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-12-11T01:04:16</lastRun>
+   <lastRun>2018-12-12T22:45:42</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
@@ -16,32 +16,5 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/DragNDrop</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>39ffd7de-8940-4be3-b71f-c92951daa2c3</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/OHRMTests/LoginValidation</testCaseId>
-      <testDataLink>
-         <combinationType>MANY</combinationType>
-         <id>bcec501d-eea3-4d7a-bcaf-6fcd76ba6ce2</id>
-         <iterationEntity>
-            <iterationType>ALL</iterationType>
-            <value></value>
-         </iterationEntity>
-         <testDataId>Data Files/TestData</testDataId>
-      </testDataLink>
-      <variableLink>
-         <testDataLinkId>bcec501d-eea3-4d7a-bcaf-6fcd76ba6ce2</testDataLinkId>
-         <type>DATA_COLUMN</type>
-         <value>usename</value>
-         <variableId>a9d0aff2-0f40-4528-8055-221f99b0665e</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId>bcec501d-eea3-4d7a-bcaf-6fcd76ba6ce2</testDataLinkId>
-         <type>DATA_COLUMN</type>
-         <value>password</value>
-         <variableId>dc3ca51e-42a1-4947-a5f6-72a03dd2b5c7</variableId>
-      </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/TestSuite1.ts
+++ Test Suites/TestSuite1.ts
@@ -4,17 +4,11 @@
    <name>TestSuite1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-11-28T01:32:54</lastRun>
+   <lastRun>2018-12-12T22:31:38</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>49e7e209-c223-43a8-b376-798108475f00</testSuiteGuid>
-   <testCaseLink>
-      <guid>090ec08b-0f4c-4970-b4ca-44a58fb7b698</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/DragNDrop</testCaseId>
-   </testCaseLink>
 </TestSuiteEntity>
